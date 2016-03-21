8dc9a8820b042c8595841f3e74de32839efbcd72
limingth <limingth@gmail.com>
2016-03-21 16:56:32 +0800

    Upgrade version form 0.0.1 to 0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'maodouio:posts-summernote',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Maodou.io posts package with summernote, A meteor package that provides posts pages at /posts',
   // URL to the Git repository containing the source code for this package.
