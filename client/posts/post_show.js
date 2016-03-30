50db6e7ad858b70d78f9ed7c7853cf0b09745aa4
zhaoic <zhaoic@163.com>
2016-03-30 12:05:23 +0800

    add posts share in wx

--- client/posts/post_show.js
+++ client/posts/post_show.js
@@ -81,3 +81,66 @@ AutoForm.hooks({
     }
   }
 });
+
+Template.postShow.onRendered(function() {
+	var share_config = {
+       "share": {
+          "imgUrl": $(".postImg > img")[0].src,
+          "desc" : this.data.post.description,
+          "title" : this.data.post.title,
+          "link": window.location.href,
+          "success":function(){
+            //分享成功后的回调函数
+          },
+          'cancel': function () {
+            // 用户取消分享后执行的回调函数
+          }
+      }
+  };
+
+  var url = "";
+  var current = Iron.Location.get();
+
+  if (current.host === "") {
+    // route 过来的地址，微信只能获取到 /activities 截止，后面的取不到了
+    url = window.location.origin + "/posts/";
+  } else {
+    // 刷新页面或者新建文章后跳转的页面，微信获取的是完整地址
+    url = window.location.href;
+  }
+
+  // 根据不同情况传递不同的地址获取 signature
+  Meteor.call("signature", url, function(error, result) {
+    console.log(result.signature);
+    Meteor.call("printLog", result.signature);
+
+    wx.config({
+      debug: false,
+      appId: result.appId,
+      timestamp: result.timestamp,
+      nonceStr: result.nonceStr,
+      signature: result.signature,
+      jsApiList: [
+        'checkJsApi',
+        'onMenuShareTimeline',
+        'onMenuShareAppMessage',
+        'onMenuShareQQ',
+        'onMenuShareWeibo',
+        'hideMenuItems',
+        'chooseImage',
+        'uploadImage',
+				'scanQRCode',
+      ]
+    });
+
+    wx.ready(function () {
+      wx.onMenuShareAppMessage(share_config.share);
+      wx.onMenuShareTimeline(share_config.share);
+      wx.onMenuShareQQ(share_config.share);
+    });
+
+    wx.error(function(res){
+      Meteor.call("printLog", "wx.error :", res.errMsg);
+    });
+  });
+});
