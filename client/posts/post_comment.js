c99113f69f935f112460b077525e9c35fae81645
Tricrepe <644568681@qq.com>
2016-04-05 19:38:20 +0800

    delete wasting lines

--- client/posts/post_comment.js
+++ client/posts/post_comment.js
@@ -1,9 +1,6 @@
 Template.postComment.events ({
   "click #js_cmt_submit": function(event, template){
-    console.log('input');
     var comment_input = $('#js_cmt_input').val();
-    console.log(comment_input);
-    console.log(this.post);
     Comment.collection.insert({
       linkedObjectId: this.post._id,
       userId: Meteor.userId(),
