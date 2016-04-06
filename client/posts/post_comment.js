47c5edff631cfabea327c619485fd25cfc9c178e
limingth <limingth@gmail.com>
2016-04-06 09:38:14 +0800

    Add login before summit comments

--- client/posts/post_comment.js
+++ client/posts/post_comment.js
@@ -1,11 +1,20 @@
 Template.postComment.events ({
   "click #js_cmt_submit": function(event, template){
-    var comment_input = $('#js_cmt_input').val();
-    Comment.collection.insert({
-      linkedObjectId: this.post._id,
-      userId: Meteor.userId(),
-      body: comment_input
-    })
-    Router.go("/posts/"+this.post._id);
+    if (Meteor.user()) {
+      // console.log("activity :", activity);
+      var comment_input = $('#js_cmt_input').val();
+      Comment.collection.insert({
+        linkedObjectId: this.post._id,
+        userId: Meteor.userId(),
+        body: comment_input
+      })
+      Router.go("/posts/"+this.post._id);
+      } else {
+        if (confirm("请您登录后再提交评论!")) {
+          var post = Posts.findOne();
+          window.location.href = "/userLogin?logintype=/posts/" + this.post._id;
+          console.log("跳转成功!");
+      }
+    }
   },
 })
